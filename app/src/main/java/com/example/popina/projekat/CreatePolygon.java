1dc88aa2c87f3064c8e4f1b99c2f8f7b049e9ea9
Djordje Zivanovic <djordjezivanovicva@gmail.com>
2017-03-03 14:42:20 +0100

    Added settings for app.

---
+++ app/src/main/java/com/example/popina/projekat/CreatePolygon.java
@@ -0,0 +1,13 @@
+package com.example.popina.projekat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreatePolygon extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_polygon);
+    }
+}
